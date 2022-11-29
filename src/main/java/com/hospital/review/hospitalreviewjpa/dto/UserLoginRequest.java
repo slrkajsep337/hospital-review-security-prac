dc412f1f7df075e9988ccf9594c11fa0a0a61d33
이수진 <slrkajsep337@naver.com>
2022-11-29 23:58:14 +0900

    login dto 생성

---
+++ src/main/java/com/hospital/review/hospitalreviewjpa/dto/UserLoginRequest.java
@@ -0,0 +1,15 @@
+package com.hospital.review.hospitalreviewjpa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserLoginRequest {
+    private String userName;
+    private String password;
+}
+
+
