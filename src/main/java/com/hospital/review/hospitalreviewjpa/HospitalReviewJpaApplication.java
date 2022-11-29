5881b402e544fefd7557d23d46e8b159abf58c7b
이수진 <slrkajsep337@naver.com>
2022-11-30 00:00:11 +0900

    initial commit

---
+++ src/main/java/com/hospital/review/hospitalreviewjpa/HospitalReviewJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.review.hospitalreviewjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalReviewJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalReviewJpaApplication.class, args);
+    }
+
+}
