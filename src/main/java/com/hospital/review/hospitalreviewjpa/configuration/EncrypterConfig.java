3ef737b63812e6d3af2c458049e705394f865310
이수진 <slrkajsep337@naver.com>
2022-11-29 23:54:08 +0900

    encodePassword 메소드 추가

---
+++ src/main/java/com/hospital/review/hospitalreviewjpa/configuration/EncrypterConfig.java
@@ -0,0 +1,17 @@
+package com.hospital.review.hospitalreviewjpa.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class EncrypterConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder(); // 패스워드를 인코딩 할 때 사용
+    }
+}
+가
