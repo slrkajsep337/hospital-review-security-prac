6d51568fe425520cd1f126531cde1a5af0a5d27a
이수진 <slrkajsep337@naver.com>
2022-11-29 23:49:37 +0900

    not_found, invalid_password 추가

--- src/main/java/com/hospital/review/hospitalreviewjpa/exception/ErrorCode.java
+++ src/main/java/com/hospital/review/hospitalreviewjpa/exception/ErrorCode.java
@@ -10,7 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     //enum - 들어갈 수 있는 값들을 미리 지정해두어서 다른 값은 못들어가게함. 예측한 범위 내에서만 프로그램이 작동하도록 하는 기능
 
-    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated.");
+    DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "UserName Not Found."),
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "password is not correct") ;
+
 
     private HttpStatus status;
     private String message;
