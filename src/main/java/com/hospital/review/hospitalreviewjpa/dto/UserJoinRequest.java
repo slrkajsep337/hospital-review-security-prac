afe0b20649305839b8f5a8e29ed21fdfe572396b
이수진 <slrkajsep337@naver.com>
2022-12-05 23:30:56 +0900

    userRole 추가

--- src/main/java/com/hospital/review/hospitalreviewjpa/dto/UserJoinRequest.java
+++ src/main/java/com/hospital/review/hospitalreviewjpa/dto/UserJoinRequest.java
@@ -2,6 +2,7 @@ package com.hospital.review.hospitalreviewjpa.dto;
 
 
 import com.hospital.review.hospitalreviewjpa.domain.User;
+import com.hospital.review.hospitalreviewjpa.domain.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,8 +22,9 @@ public class UserJoinRequest { //회원가입 때 요청받을 정보
     public User toEntity(String password) {
         return User.builder()
                 .userName(this.userName)
-                .password(password)
+                .password(password) //password를 그대로 넣는것이 아니라, 복호화를 해서 넣음
                 .emailAddress(this.email)
+                .role(UserRole.USER) //role 추가
                 .build();
     }
 }
