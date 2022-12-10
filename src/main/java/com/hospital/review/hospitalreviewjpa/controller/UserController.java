fbf72bf5fc4d7050e9892ee8e1755fdeed24cf83
이수진 <slrkajsep337@naver.com>
2022-12-10 21:06:18 +0900

    반환 타입 변경(Response->ResponseEntity)

--- src/main/java/com/hospital/review/hospitalreviewjpa/controller/UserController.java
+++ src/main/java/com/hospital/review/hospitalreviewjpa/controller/UserController.java
@@ -6,6 +6,7 @@ import com.hospital.review.hospitalreviewjpa.dto.*;
 import com.hospital.review.hospitalreviewjpa.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,19 +21,28 @@ public class UserController {
 
     private final UserService us;
 
+//    @PostMapping("/join")
+//    //response entity가 아닌 response 타입으로 반환
+//    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest) {
+//        UserDto userDto = us.join(userJoinRequest);
+//        return Response.success(new UserJoinResponse(userDto.getUserName(),userDto.getEmail()));
+//    }
+
     @PostMapping("/join")
-    //response entity가 아닌 response 타입으로 반환
-    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest) {
-        UserDto userDto = us.join(userJoinRequest);
-        return Response.success(new UserJoinResponse(userDto.getUserName(),userDto.getEmail()));
+    public ResponseEntity join(@RequestBody UserJoinRequest dto) {
+        us.join(dto);
+        return ResponseEntity.ok().body("회원가입에 성공했습니다");
     }
+
+//    @PostMapping("/login")
+//    public Response<UserLoginResponse> login(@RequestBody UserLoginRequest userLoginRequest) {
+//        String token = us.login(userLoginRequest.getUserName(), userLoginRequest.getPassword());
+//        return Response.success(new UserLoginResponse(token));
+//    }
 
     @PostMapping("/login")
-    public Response<UserLoginResponse> login(@RequestBody UserLoginRequest userLoginRequest) {
-        String token = us.login(userLoginRequest.getUserName(), userLoginRequest.getPassword());
-        return Response.success(new UserLoginResponse(token));
+    public ResponseEntity login(@RequestBody UserLoginRequest dto) {
+        String token = us.login(dto.getUserName(), dto.getPassword());
+        return ResponseEntity.ok().body(token);
     }
-
-
-
 }
